82f718eac24314fa3f84dffcf39d95e98b65c61d
Markus Olsen <molsen13@gmail.com>
2015-06-02 16:26:27 -0600

    Abandoned handlebars and gulp

--- test/board-test.js
+++ test/board-test.js
@@ -3,7 +3,7 @@ var Board = require('../lib/board.js');
 
 describe('Board', function() {
   describe('#svgBody', function() {
-    xit('returns a string of html to draw the rect with the input x and y coordinates', function() {
+    it('returns a string of html to draw the rect with the input x and y coordinates', function() {
       expect(Board.svgBody(3, 4)).to.eq('<rect x="3" y="4" width="50" height="50" stroke="red" stroke-width="3"/>');
     });
   });
