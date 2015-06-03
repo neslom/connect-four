2358d38dc59df9e327dcbe039be2d6439a55673c
Markus Olsen <molsen13@gmail.com>
2015-06-03 15:26:43 -0600

    test working??

--- test/test.js
+++ test/test.js
@@ -1,6 +1,2 @@
-const Board = require('../lib/board.js');
-const findColumn = require('../lib/columns.js');
-const circleEmpty = require('../lib/columns.js');
-const $ = require('jquery');
-
-Board.drawBoard();
+require('./board-test.js')
+require('./column-test.js')
