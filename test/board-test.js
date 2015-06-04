80aa993f780a07e6196d272314ca6e26ca7aa94d
Markus Olsen <molsen13@gmail.com>
2015-06-03 21:31:22 -0600

    Board test passing when calling Board.drawBoar() before initial state test

--- test/board-test.js
+++ test/board-test.js
@@ -1,19 +1,19 @@
-var expect = require('chai').expect;
-var Board = require('../lib/board.js');
-var $ = require("jquery");
+const expect = require('chai').expect;
+import Board from '../lib/board.js';
+import $ from 'jquery';
 
 describe('Board', function() {
   describe('#svgBody', function() {
     it('returns a string of html to draw the rect with the input x and y coordinates', function() {
-      expect(2).to.eq(2);
       expect(Board.svgBody(3, 4)).to.eq('<rect x="3" y="4" width="50" height="50" stroke="red" stroke-width="3"/>');
     });
   });
 
-  describe('In initial state', function(){
+  describe('in initial state', function(){
+    Board.drawBoard();
     it('displays an empty board', function(){
-      expect($(".filled").length).to.eq(0);
-      expect($("rect").length).to.eq(42);
+      expect($('.filled').length).to.eq(0);
+      expect($('rect').length).to.eq(42);
     });
   });
 });
